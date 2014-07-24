5d8dade3aa7d0e19e047a399f3fd911602368b23
Aaron Antrim <aaron@trilliumtransit.com>
2014-07-24 12:45:56 -0700

    Added links to new timetables -- becoming effective July 27

--- route-7.php
+++ route-7.php
@@ -108,10 +108,12 @@ Template Name: route-7-9
 				Saturday: 10 a.m. – 5 p.m.
 			</div> <!-- end #days-of-week -->
 			<div id="schedule-buttons">
+				
+				<!-- links to North- and South-bound timetables, ending July 25, 2014; remove these on Monday -->
 				<a href="/routes/routes-7-and-9/route-9-northbound-mon-fri-aug-2013/"  >
 				<div id="schedule-northbound-65" class="route-popup-button route-button-left route-button-first route-button-odd route-button-short" style="height:45px;">
 					<div class="popup-button-title">
-					Northbound Mon-Fri
+					Northbound Mon-Fri<br/>(effective through July 25, 2014)
 					<br/><span style="font-size:12px;"</span>
 					</div> <!-- end #popup-button-title -->
 					<div class="popup-button-desc" >
@@ -122,7 +124,32 @@ Template Name: route-7-9
 				<a href="/routes/routes-7-and-9/route-9-southbound-mon-fri-aug-2013/"  >
 				<div id="schedule-southbound-65" class="route-popup-button route-button-right route-button-even route-button-short " style="height:45px;">
 				<div class="popup-button-title">
-					Southbound Mon-Fri
+					Southbound Mon-Fri<br/>(effective through July 25, 2014)
+					<br/><span style="font-size:12px;"</span>
+					</div> <!-- end #popup-button-title -->
+					<div class="popup-button-desc">
+
+					</div> <!-- end popup-button-desc -->
+				</div> <!-- end #schedule-southbound-65-->
+				</a>
+
+				<!-- links to new timetables effective July 27, 2014 -->
+
+					<a href="/routes/routes-7-and-9/route-9-northbound-monday-friday-effective-27-july-2014/"  >
+				<div id="schedule-northbound-65" class="route-popup-button route-button-left route-button-first route-button-odd route-button-short" style="height:45px;">
+					<div class="popup-button-title">
+					Northbound Mon-Fri<br/>(beginning July 27, 2014)
+					<br/><span style="font-size:12px;"</span>
+					</div> <!-- end #popup-button-title -->
+					<div class="popup-button-desc" >
+
+					</div> <!-- end popup-button-desc -->
+				</div> <!-- end #schedule-northbound-9 -->
+				</a>
+				<a href="/routes/routes-7-and-9/route-9-southbound-monday-friday-effective-27-july-2014/"  >
+				<div id="schedule-southbound-65" class="route-popup-button route-button-right route-button-even route-button-short " style="height:45px;">
+				<div class="popup-button-title">
+					Southbound Mon-Fri<br/>(beginning July 27, 2014)
 					<br/><span style="font-size:12px;"</span>
 					</div> <!-- end #popup-button-title -->
 					<div class="popup-button-desc">
