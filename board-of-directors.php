402ed812d7c2d7d23ef45a63fa96aa2c00c5d6d9
Nome Q <dickerson.nomi@gmail.com>
2019-05-24 17:48:44 -0700

    update board page code

--- board-of-directors.php
+++ board-of-directors.php
@@ -1,74 +1,65 @@
 <?php
 /**
-Template Name: board of directors
+* Template Name: board of directors
  */
 
 get_header(); ?>
 <div id="page-overlay">
-		<?php the_post(); ?>
+	<?php the_post(); ?>
 
 
 
-			<h1 class="entry-title main"><?php the_title(); ?></h1>
-<?php //if(function_exists(simple_breadcrumb)) {simple_breadcrumb();} ?>
+	<h1 class="entry-title main"><?php the_title(); ?></h1>
 <br/>
 <hr />
-                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-
-<?php if (true ) {
+<?php
+$today = date('Ymd');
+$upcoming = new WP_Query(array(
+     'category_name' => 'board-of-directors',
+     'meta_key' => 'board-info-date',
+     'orderby' => 'meta_value_num',
+     'order' => 'ASC',
+     'posts_per_page' => -1,
+     'meta_query' => array(
+          array(
+               'key' => 'board-info-date',
+               'value'	=> $today,
+               'compare' => '>=',
+               'type'	=> 'NUMERIC'
+          )
+     )
+));
 ?>
-<h2>Upcoming Meetings and Agendas</h2>	<?php
-		query_posts( array ( 'category_name' => 'board-of-directors', 'posts_per_page' => -1 ) );
-		// post upcoming
-		 echo '<ul class="upcoming-agendas">';
-		 $count = 0;
-while ( have_posts() ) : the_post();
+<h2>Upcoming Meetings and Agendas</h2>
+<?php if ($upcoming->have_posts()) : ?>
+     <ul class="upcoming-agendas">
+     <?php while ($upcoming->have_posts()) :
+          $upcoming->the_post();
+          $linkText = " ";
+	     //if has field pdf && field description is empty
 
-        $current_time = current_time('mysql');
+	     $pdfText = get_field("agenda-packet-pdf");
 
-        $post_time = get_post_meta($post->ID, 'wpcf-board-info-date', true);
-		$current_time = current_time('timestamp', $gmt = 0);
+	     if(!empty($post->post_content)) {
+		     $linkText = get_permalink();
+	     } else if ($pdfText != "") {
+	 	     $linkText = $pdfText;
+	     } else {
+               $linkText = get_permalink();
+          }
+     ?>
+          <li><a href="<?php echo $linkText; ?>">
+               <?php the_field('board-info-date'); ?> &mdash; <?php the_title(); ?>
+          </a></li>
 
+     <?php endwhile; wp_reset_postdata(); ?>
+     </ul>
+<? else : ?>
+     <p>No upcoming events listed.</p>
+<?php endif; ?>
 
-     if ($post_time >= $current_time) {
-	  $count += 1;
-
-	  //make link text
-	  $linkText = " ";
-	  //if has field pdf && field description is empty
-
-	  $pdfText = get_field("agenda-packet-pdf");
-
-	  if(!empty($post->post_content)) {
-		  $linkText = get_permalink();
-	  } else if ($pdfText != "") {
-	 	 $linkText = $pdfText;
-	  } else {
-	  	$linkText = get_permalink();
-	  			  echo "3";
-	  }
-
-      echo "<li>";
-      //echo "date: ".$post_time.", current: ".$current_time."<br />";
-      echo '<a href="';
-      echo $linkText;
-      echo'">';
-	  echo get_field('board-info-date');
-      echo ' - ';
-      echo the_title();
-      echo '</a>';
-      echo "</li>";
-     }
-
-endwhile;
-
-if ($count == 0) {
-     echo 'There are no upcoming events that are listed.';
-     }
-
-echo '</ul>';
-?>
 
 <h2> Past Agendas and Minutes </h2>
 
@@ -128,7 +119,7 @@ endwhile;
 echo '</ul>';
 
  wp_reset_query();
-}; // closes login
+
 
 
 
