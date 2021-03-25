7f8d2e47a9626f6ba29cb43ef46b6eae49e744ce
ekeaton <e.keaton@live.com>
2021-03-25 11:18:39 -0700

    archives for alerts and news

---
+++ alerts.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Template Name: alerts
+*/
+ get_header(); ?>
+
+  <div id="page-overlay">
+    <?php the_post(); ?>
+    <header>
+      <h1 class="entry-title main"><?php the_title(); ?></h1>
+    </header>
+ 
+			<br/>
+			<hr />
+    <main>
+    <div id="post-&lt;?php the_ID(); ?&gt;" <?php post_class(); ?>>
+     
+
+  
+
+      <div class="entry-content">
+
+
+       
+
+         <?php the_content(); ?><?php
+        // The Query
+        $alerts = new WP_Query(array(
+          'post_type' => array('alert'),
+          'posts_per_page' => 3
+        ));
+        ?><?php
+        // The Loop
+        while ( $alerts->have_posts() ) : $alerts->the_post();
+
+                   echo '<div class="archive-alert-item">';
+                echo '<a href="';
+                echo the_permalink();
+                echo '">';
+                echo the_date();
+               echo ' - ';
+
+
+                echo the_title();
+
+
+
+
+               echo '</a>';
+               echo '</div>';
+        endwhile;
+        // Reset Query
+
+        global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
+
+        <div id="nav-above" class="navigation">
+          <div class="nav-previous">
+            <?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older news articles', 'your-theme' )) ?>
+          </div>
+
+          <div class="nav-next">
+            <?php previous_posts_link(__( 'Newer news articles <span class="meta-nav">&raquo;</span>', 'your-theme' )) ?>
+          </div>
+        </div><!-- #nav-above -->
+        <?php }
+        wp_reset_query();
+        ?>
+      </div>
+    </main>
+    
+  </div><!-- page-overlay -->
+  <?php get_footer(); ?><?php edit_post_link( __( 'Edit', 'your-theme' ), '<span class="edit-link">', '</span>' ) ?>
+  </div><!-- .entry-content -->
+
+  <?php get_footer(); ?>
--- news.php
+++ news.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: news-archive
+Template Name: news
 */
  get_header(); ?>
 
@@ -9,58 +9,19 @@ Template Name: news-archive
     <header>
       <h1 class="entry-title main"><?php the_title(); ?></h1>
     </header>
-    <?php //if(function_exists(simple_breadcrumb)) {simple_breadcrumb();} ?>
+ 
 			<br/>
 			<hr />
     <main>
     <div id="post-&lt;?php the_ID(); ?&gt;" <?php post_class(); ?>>
-       <h2>Current Alerts</h2>
-          <?php the_content(); ?><?php
-        // The Query
-        $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        query_posts("paged=$page&cat=4");
-        ?><?php
-        // The Loop
-        while ( have_posts() ) : the_post();
+     
 
-
-                echo '<div class="archive-alert-item">';
-                echo '<a href="';
-                echo the_permalink();
-                echo '">';
-                echo the_date();
-               echo ' - ';
-
-
-                echo the_title();
-
-
-
-
-               echo '</a>';
-               echo '</div>';
-        endwhile;
-        // Reset Query
-
-        global $wp_query; $total_pages = $wp_query->max_num_pages; if ( $total_pages > 1 ) { ?>
-
-        <div id="nav-above" class="navigation">
-          <div class="nav-previous">
-            <?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> Older news articles', 'your-theme' )) ?>
-          </div>
-
-          <div class="nav-next">
-            <?php previous_posts_link(__( 'Newer news articles <span class="meta-nav">&raquo;</span>', 'your-theme' )) ?>
-          </div>
-        </div><!-- #nav-above -->
-        <?php }
-        wp_reset_query();
-        ?>
+  
 
       <div class="entry-content">
 
 
-         <h2>News Archive</h2>
+       
 
          <?php the_content(); ?><?php
         // The Query
